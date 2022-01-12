48abec3c7620e6ede730219203a3fb2d176e5b07
lijianhao <1498158052@qq.com>
2022-01-12 22:26:59 +0800

    inject 更新完成

--- injectWatch/index.js
+++ injectWatch/index.js
@@ -9,8 +9,8 @@ const parcelRunArg = process.argv[2]
 const root = dirname(__dirname)
 const injectDir = join(root, '/src/inject')
 const watchFileType = ['js', 'ts', 'css', 'less']
-// const watchFileType = ['ts', 'css']
-// 生成文件系统
+
+// 生成 dirArray
 const dirArray = []
 function generateDirArray(dir){
     dirArray.push(dir)
@@ -26,34 +26,25 @@ generateDirArray(injectDir)
 
 /**
  * 生成 [npx parcel ... F:\\chrome-test\\chrome-extensions-template\\src\\inject\\*.ts --dist-dir F:\\chrome-test\\chrome-extensions-template\\dist\\inject\\]
- * 
  * */ 
-const commandArray = dirArray.reduce((prev, dirPath)=>{
+function generateCommad(dirPath){
     defultPort+=1
     const enterPath = watchFileType.reduce((prev, type)=>{
-            prev += ` ${dirPath}\\*.${type}`
-            return prev
-        }, '')
+        prev += ` ${dirPath}\\*.${type}`
+        return prev
+    }, '')
     const addparcelHmrPort = parcelRunArg === 'watch' ? `--hmr-port ${defultPort}` : ''
     const distPath = dirPath.replace('src', 'dist')
     const after = enterPath + ` --dist-dir ${distPath} ${addparcelHmrPort}`
-    const command = `npx parcel ${parcelRunArg}` + after
-    prev.push(command.replace(/\\/g, '\\\\'))
+    return (`npx parcel ${parcelRunArg}` + after).replace(/\\/g, '\\\\')
+}
+const commandArray = dirArray.reduce((prev, dirPath)=>{
+    prev.push(generateCommad(dirPath))
     return prev
 },[])
 
-// console.log(commandArray)
-// watch(injectDir, {
-//     recursive: true
-// }).on('change', function(evt, path){
-//     if(path.includes('.')){
-//         // 如果是目录
-        
-//     }
-// })
-
-commandArray.forEach((command)=>{
-    console.log(command)
+// 运行子程序
+function runChildShell(command){
     const processExec = child_process.exec(`${command}`, {})
     processExec.stdout.on('data', data=>{
         console.log(data.toString())
@@ -61,4 +52,21 @@ commandArray.forEach((command)=>{
     processExec.stderr.on('data', data=>{
         console.log(data.toString())
     })
-})
+
+}
+commandArray.forEach((command)=> runChildShell(command) )
+
+if(parcelRunArg === 'watch'){
+    // 监听 inject 文件夹创建文件夹 进行 parcel 监听
+    watch(injectDir, {
+        recursive: true
+    }).on('change', function(evt, path){
+        if(!path.includes('.') && !dirArray.includes(path)){
+            // 如果是目录，并且内部不存在。防止重复
+            dirArray.push(path)
+            runChildShell(
+                generateCommad(path)
+            )
+        }
+    })
+}
