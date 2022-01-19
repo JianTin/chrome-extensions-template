47074d856a29db97d421096aaf12f1fe2e0d0dab
lijianhao <1498158052@qq.com>
2022-01-19 18:40:00 +0800

    增加 i18n 支持

--- src/action/index.ts
+++ src/action/index.ts
@@ -2,4 +2,7 @@ import {render} from 'react-dom'
 import {createElement} from 'react'
 import App from './app'
 
+console.log(
+    chrome.i18n.getMessage('name')
+)
 render(createElement(App), document.getElementById('app'))
--- watch/static.js
+++ watch/static.js
@@ -7,10 +7,12 @@ const root = dirname(__dirname)
 // enter
 const manifestPath = join(root, 'manifest.json')
 const assetsPath = join(root, 'assets')
+const locales = join(root, 'src/_locales')
 // output
 const distPath = join(root, 'dist')
 const distManifest = join(distPath, 'manifest.json')
 const distAsset = join(distPath, 'assets')
+const distLocales = join(distPath, '_locales')
 
 watchFile(manifestPath, ()=>{
     removeSync(distManifest)
@@ -19,4 +21,8 @@ watchFile(manifestPath, ()=>{
 watch(assetsPath, {recursive: true},()=>{
     removeSync(distAsset)
     copySync(assetsPath, distAsset)
+})
+watch(locales, {recursive: true}, ()=> {
+    removeSync(distLocales)
+    copySync(locales, distLocales)
 })
